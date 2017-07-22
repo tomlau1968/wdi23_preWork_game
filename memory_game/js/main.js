f4f63812a809898753ad9fe44fddfb2b4f799e99
tomtom <info@tomlau.com.au>
2017-07-23 06:19:57 +1000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -10,3 +10,13 @@ var cardTwo = card[2];
 cardsInPlay.push("cardTwo");
 console.log("User flipped king");
 
+ if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!");
+  } else {
+      alert("Sorry, try again.");
+  }
+	
+
+
+
+
